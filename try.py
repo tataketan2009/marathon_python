4b5140955dc13bf39a74a121fecd590b6ef9887f
tataketan2009 <tataketan2009@gmail.com>
2026-07-03 19:47:43 +0530

    .\try.py

--- try.py
+++ try.py
@@ -1,9 +1,1 @@
-import random
-
-num = random.randint(1, 100)
-
-print("Welcome to the Number Guessing Game!")
-
-guess = int(input("Guess a number between 1 and 100: "))
-
-print("good morning")
+print("Hello world")
