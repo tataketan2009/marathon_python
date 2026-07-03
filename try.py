aafe41ce6fe20f4e6fd7998def0ceae1ccb08e81
tataketan2009 <tataketan2009@gmail.com>
2026-07-03 08:30:14 +0530

    .\try.py

--- try.py
+++ try.py
@@ -6,12 +6,3 @@ print("Welcome to the Number Guessing Game!")
 
 guess = int(input("Guess a number between 1 and 100: "))
 
-while guess != num:
-    if guess < num:
-        print("Too low! Try again.")
-    elif guess > num:
-        print("Too high! Try again.")
-    guess = int(input("Guess a number between 1 and 100: "))
-    print("Congratulations! You guessed the correct number:", num) 
-else:
-    print("Invalid input. Please enter a number between 1 and 100.")
