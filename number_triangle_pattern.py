e81a95fe49fa7e69f8d4e8f922d1c4d848b69849
tataketan2009 <tataketan2009@gmail.com>
2026-07-04 05:26:37 +0530

    .\number_triangle_pattern.py

--- number_triangle_pattern.py
+++ number_triangle_pattern.py
@@ -13,3 +13,12 @@ for i in range(1, n + 1):
 # 4 5 6
 # 7 8 9 10
 # 11 12 13 14 15
+
+n = 5 
+num = 100
+
+for i in range(n +1):
+    for j in range(i-1):
+        print(num + 1, end= " ")
+        num += 1
+    print()
