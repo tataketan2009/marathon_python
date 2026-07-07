f9d617119d2471998ddae3251517b394815b3f24
tataketan2009 <tataketan2009@gmail.com>
2026-07-07 17:38:08 +0530

    .\while_and_for_loops.py

--- while_and_for_loops.py
+++ while_and_for_loops.py
@@ -6,3 +6,13 @@ i = 0
 while i <= 10:
     print(i, end=" ")
     i += 1
+
+
+for i in range(0, 101):
+    if i % 2 == 0:
+        print(i, end=" ")
+
+
+for i in range(0, 101):
+    if i % 2 != 0:
+        print(i, end=" ")
