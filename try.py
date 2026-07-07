8e22e2df1e38fe81a2fe292ff9b6e9c750102730
tataketan2009 <tataketan2009@gmail.com>
2026-07-07 17:37:41 +0530

    .\try.py

--- try.py
+++ try.py
@@ -1,32 +1,3 @@
-import random
+for i in range(0, 11):
+    print(i, "*",i,"=",  i*i)
 
-while True:  
-    num = random.randint(1, 100)
-    chances = 10  
-    attempts = 0  
-    game_won = False  
-
-    print("\n--- New Game Started ---")
-    print("You have 10 chances to guess the number!")
-
-    while attempts < chances:
-        guess = int(input(f"Attempt {attempts + 1}/{chances} - Guess a number between 1 and 100: "))
-        attempts += 1  
-
-        if guess == num:
-            print("Congratulations! You guessed the number.")
-            game_won = True
-            break
-        elif guess < num:
-            print("Too low! Try again.")
-        elif guess > num:
-            print("Too high! Try again.")
-
-    if not game_won:
-        print(f"Game over! You've run out of chances. The correct number was {num}.")
-
-    
-    play_again = input("\nDo you want to play another game? (yes/no): ").strip().lower()
-    if play_again != 'yes':
-        print("Thanks for playing! Goodbye.")
-        break  
