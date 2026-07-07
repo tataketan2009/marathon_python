a10d17ac3349014da7e96856d5c3e3b6350439bd
tataketan2009 <tataketan2009@gmail.com>
2026-07-07 16:58:37 +0530

    .\number_triangle_pattern.py

--- number_triangle_pattern.py
+++ number_triangle_pattern.py
@@ -1,24 +1,6 @@
-n = 5 
-num = 1
+num = 7
+n = 7
 
-for i in range(1, n + 1):
-    for j in range(i):
-        print(num, end= " ")
-        num += 1
-    print()
-
-#this code gives the output as follows:
-# 1
-# 2 3
-# 4 5 6
-# 7 8 9 10
-# 11 12 13 14 15
-
-n = 5 
-num = 100
-
-for i in range(n +1):
-    for j in range(i-1):
-        print(num + 1, end= " ")
-        num += 1
-    print()
+for i in range(1, num+1):
+    for j in  range(i + 1):
+        print(j * "*")
