106cbdabb30ad065ec104e91e13503de0356536c
tataketan2009 <tataketan2009@gmail.com>
2026-07-07 17:58:12 +0530

    .\while_and_for_loops.py

--- while_and_for_loops.py
+++ while_and_for_loops.py
@@ -16,3 +16,11 @@ for i in range(0, 101):
 for i in range(0, 101):
     if i % 2 != 0:
         print(i, end=" ")
+
+
+
+
+l =  ['banana', 'orange', 'mango', 'lemon'] 
+
+for l in reversed(l):
+    print(l, end=" ")
