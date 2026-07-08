df51bb16d89a3452e7d5aa8eb0996c0820c5a47f
tataketan2009 <tataketan2009@gmail.com>
2026-07-08 18:17:05 +0530

    .\Functions.py

--- Functions.py
+++ Functions.py
@@ -8,3 +8,20 @@ a = 5
 
 anumswer = number(a,)
 print(anumswer)
+
+
+def reverse_my_list(user_list):
+    user_list.reverse()  
+    return user_list     
+
+
+user_input = input("Enter a list of items separated by spaces: ")
+
+
+my_list = user_input.split()
+
+
+reversed_result = reverse_my_list(my_list)
+
+
+print("Reversed list:", reversed_result)
