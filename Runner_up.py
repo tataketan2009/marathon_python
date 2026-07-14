d0d1dba6622ebc808fe25ca2f101f3948a4e3d0c
tataketan2009 <tataketan2009@gmail.com>
2026-07-15 05:27:51 +0530

    .\Runner_up.py

---
+++ Runner_up.py
@@ -0,0 +1,21 @@
+print("good morning")
+
+n = int(input("Enter the number of players: "))
+
+score = []
+
+for i in range(n):
+    player_score = int(input(f"Enter the score of player {i + 1}: "))
+    if player_score > 6:
+        print("Score cannot be greater than 6. Please enter a valid score.")
+        player_score = int(input(f"Enter the score of player {i + 1}: "))
+    score.append(player_score)
+
+print("Scores:", score)
+
+
+score.sort()
+print("Sorted Scores:", score)
+
+runner = score[-2]
+print("Runner-up score:", runner)
